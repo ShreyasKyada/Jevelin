223c7433f3ae969509c7c962cdb7fdb341905ddf
ShreyasKyada <83200751+ShreyasKyada@users.noreply.github.com>
2021-07-28 18:39:33 +0530

    shreyas

--- JS/index.js
+++ JS/index.js
@@ -21,3 +21,28 @@ btn.addEventListener('click', () => {
         click = false;
     }
 });
+
+const homeCard1 = document.getElementsByClassName('home-card')[0];
+const homeCard2 = document.getElementsByClassName('home-card')[1];
+const homeCard3 = document.getElementsByClassName('home-card')[2];
+const homeCardContainer = document.getElementsByClassName('home-card-container')[0];
+
+homeCardFunction = () => {
+    if(homeCard1.classList.contains('down')){
+        homeCard1.classList.add('scroll1');
+        homeCardContainer.classList.add('home-container');
+    }
+    if(homeCard2.classList.contains('down')){
+        homeCard2.classList.add('scroll2');
+    }
+    if(homeCard3.classList.contains('down')){
+        homeCard3.classList.add('scroll3');
+    }
+
+    homeCard1.classList.add('down');
+    homeCard2.classList.add('down');
+    homeCard3.classList.add('down');
+};
+
+const homeCardObserver = new IntersectionObserver(homeCardFunction);
+homeCardObserver.observe(homeCard1);
